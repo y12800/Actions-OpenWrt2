acccbe6dc588a07cc6d6b4d1fb1f59a8144b7550
y12800 <115250636+y12800@users.noreply.github.com>
2025-12-02 10:20:26 +0800

    Update OpenWrt-24.10.sh

--- OpenWrt-24.10.sh
+++ OpenWrt-24.10.sh
@@ -50,7 +50,7 @@ git clone --depth 1 https://github.com/coolsnowwolf/luci deng-tmp1 && mv deng-tm
 sed -i 's#../../#$(TOPDIR)/feeds/luci/#g' package/deng/luci-app-ddns/Makefile
 git clone --depth 1 https://github.com/coolsnowwolf/packages deng-tmp2 && mv deng-tmp2/net/ddns-scripts package/deng/ddns-scripts
 sed -i 's#../../#$(TOPDIR)/feeds/packages/#g' package/deng/ddns-scripts/Makefile
-git clone --depth 1 https://github.com/coolsnowwolf/lede/tree/master/package/lean deng-tmp5 && mv deng-tmp5/net/ddns-scripts_aliyun package/deng/ddns-scripts_aliyun && mv deng-tmp5/net/ddns-scripts_dnspod package/deng/ddns-scripts_dnspod
+git clone --depth 1 https://github.com/coolsnowwolf/lede deng-tmp5 && mv deng-tmp5/net/ddns-scripts_aliyun package/deng/ddns-scripts_aliyun && mv deng-tmp5/net/ddns-scripts_dnspod package/deng/ddns-scripts_dnspod
 sed -i 's#../../#$(TOPDIR)/feeds/packages/#g' package/deng/ddns-scripts_aliyun/Makefile
 sed -i 's#../../#$(TOPDIR)/feeds/packages/#g' package/deng/ddns-scripts_dnspod/Makefile
 
